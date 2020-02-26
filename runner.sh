9caa9cccb04c29e67900f4c5305d5a87578e646b
Edinson Jim Sanchez <edinsonjim@gmail.com>
2020-02-26 17:50:06 +0100

    refactor: new standalone file

--- runner.sh
+++ runner.sh
@@ -2,11 +2,24 @@
 
 echo "Multichannel Service Runner"
 
-if [[ "$1" == --pull ]]
+if [[ "$1" == --build ]]
 then
     cd multichannel_service
+    echo "[INFO] Pulling multichannel_service"
     git fetch
     git pull
+
+    echo "[INFO] Overwriting standalone"
+    cp -r ./standalone.xml /opt/rh/jboss-eap-6.4/standalone/configuration/standalone.xml
+
+    echo "[INFO] Compiling package"
+    # mvn clean package -DskipTests
+    mvn clean install -DskipTests
+
+    echo "[INFO] Installing packages"
+    cp -r ./it.ifis.multichannel/multichannel.atomicservice/multichannel.atomicservice.pratica/multichannel.atomicservice.pratica.deploy/target/multichannel.atomicservice.pratica.ear /opt/rh/jboss-eap-6.4/standalone/deployments
+    cp -r ./it.ifis.multichannel/multichannel.compositeservice/multichannel.compositeservice.pratica/multichannel.compositeservice.pratica.deploy/target/multichannel.compositeservice.pratica.ear /opt/rh/jboss-eap-6.4/standalone/deployments
+
 fi
 
 echo "[INFO] Running server"
