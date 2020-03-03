f383f62855c483290efe36bb328af95e78ad8494
Edinson Jim Sanchez <edinsonjim@gmail.com>
2020-03-03 10:53:43 +0100

    chore: improve log

--- runner.sh
+++ runner.sh
@@ -9,24 +9,24 @@ REPOSITORY_NAME="multichannel_service"
 
 if [[ "$1" == --build ]]
 then
-    echo "[INFO] Overwriting standalone"
+    echo "[RUNNER] [INFO] Overwriting standalone"
     cp -r ./standalone.xml /opt/rh/jboss-eap-6.4/standalone/configuration/standalone.xml
 
     for registry in ${registries[@]}; do
-        echo "[INFO] Building ${registry}"
+        echo "[RUNNER] [INFO] Building ${registry}"
         cd "$WORKDIR/$REPOSITORY_NAME/it.ifis.multichannel/multichannel.atomicservice/multichannel.atomicservice.$registry"
         mvn clean install -DskipTests
 
         cd "$WORKDIR/$REPOSITORY_NAME/it.ifis.multichannel/multichannel.compositeservice/multichannel.compositeservice.$registry"
         mvn clean install -DskipTests
 
-        echo "[INFO] Deploying ${registry}"
+        echo "[RUNNER] [INFO] Deploying ${registry}"
         cp -r "$WORKDIR/$REPOSITORY_NAME/it.ifis.multichannel/multichannel.atomicservice/multichannel.atomicservice.$registry/multichannel.atomicservice.$registry.deploy/target/multichannel.atomicservice.$registry.ear" /opt/rh/jboss-eap-6.4/standalone/deployments
         cp -r "$WORKDIR/$REPOSITORY_NAME/it.ifis.multichannel/multichannel.compositeservice/multichannel.compositeservice.$registry/multichannel.compositeservice.$registry.deploy/target/multichannel.compositeservice.$registry.ear" /opt/rh/jboss-eap-6.4/standalone/deployments
     done
 fi
 
-echo "[INFO] Running server"
+echo "[RUNNER] [INFO] Running server"
 bash /opt/rh/jboss-eap-6.4/bin/standalone.sh
 
-echo "[INFO] Good Job!"
+echo "[RUNNER] [INFO] Good Job!"
